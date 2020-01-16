126c3ff0f21a01897927e1757f0fd0eeb3764727
Vladimir Belyakov <v.belyakov@smartcat.ai>
2020-01-17 01:31:16 +0300

    Add the Template Method pattern.

---
+++ TemplateMethod/DESWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+	public class DESWriter : FileWriter
+	{
+		private const string _key = "1234"; 
+
+		public DESWriter(string filePath)
+		{
+			Path = filePath;
+		}
+
+		protected override void DecryptFile()
+		{
+			Console.WriteLine("Decrypting the file using the DES method.");
+		}
+
+		protected override void AppendData(string data)
+		{
+			Console.WriteLine("Appending the data to a file");
+		}
+
+		protected override void EncryptFile()
+		{
+			Console.WriteLine("Encrypting the file using the DES method.");
+		}
+	}
+}
---
+++ TemplateMethod/FileWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace TemplateMethod
+{
+	public abstract class FileWriter
+	{
+		protected string Path;
+		
+		protected abstract void DecryptFile();
+
+		protected virtual void AppendData(string data)
+		{
+			using (var fs = File.Open(Path, FileMode.Append))
+			using (var tw = new StreamWriter(fs))
+				tw.Write(data);
+		}
+
+		protected abstract void EncryptFile();
+
+		public void Append(string data)
+		{
+			DecryptFile();
+			AppendData(data);
+			EncryptFile();
+		}
+	}
+}
